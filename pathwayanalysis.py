87155e1a9b23dad9963cdc36058e4f3873b8d429
bending456 <bending456@uky.edu>
2021-08-13 13:10:25 -0500

    drawer

--- pathwayanalysis.py
+++ pathwayanalysis.py
@@ -1,4 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_agraph import graphviz_layout
+from graphviz import Digraph
+from IPython.display import Image
 
 def dataextract(filename):
     
@@ -24,3 +28,28 @@ def dataextract(filename):
                     counter2 += 1
 
     return data
+
+
+def drawerForAll(data):
+    G = Digraph('G', filename='all.gv',format='png',
+              node_attr={'color': 'lightblue2', 'style': 'filled','shape':'box'})
+    start = data[data.keys[0]]
+    edge = data[data.keys[1]]
+    end = data[data.keys[2]]
+    
+    unique_node = np.unique(start + end)
+    for node in unique_node:
+        G.node(node)
+    for i,j in enumerate(start):
+        if 'up' in edge[i]:
+            G.attr('edge',color='black',style='solid',arrowhead='vee')
+        else:
+            G.attr('edge',color='red',style='dashed',arrowhead='tee')
+        G.edge(j,end[i])
+    
+    G.view()
+    G.render('all',view=True)
+    Image('all.png')
+
+    return
+
